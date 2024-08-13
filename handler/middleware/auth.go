e9e6e17f1f604642adfff773a0d0f9e3549e5271
shinnosuke-suzuki <shinnosuke.suzuki@geniee.co.jp>
2024-08-13 20:45:11 +0900

    文字列空判定の修正

--- handler/middleware/auth.go
+++ handler/middleware/auth.go
@@ -10,7 +10,7 @@ func BasicAuthMiddleware(h http.Handler, username, password string) http.Handler
 		user, pass, ok := r.BasicAuth()
 
 		// Basic認証情報がない(または空で送られくる)、またはユーザー名とパスワードが一致しない場合は401 Unauthorizedを返す
-		if !ok || len(user) == 0 || len(pass) == 0 || user != username || pass != password {
+		if !ok || user == "" || pass == "" || user != username || pass != password {
 			// WWW-Authenticate ヘッダーを設定
 			w.Header().Set("WWW-Authenticate", `Basic realm="restricted"`)
 			// 401 Unauthorized ステータスコードを設定
